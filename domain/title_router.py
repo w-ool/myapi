96316c466b2de8a07cef9cde13ac0a34de20e49c
hanwool <106064132+w-ool@users.noreply.github.com>
2023-09-07 18:52:49 +0900

    API 1차 완성

--- domain/title_router.py
+++ domain/title_router.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from sentence_transformers import SentenceTransformer, util
 from database import SessionLocal
 from models import Title
 import json
 import numpy as np
+import torch
 
 router = APIRouter(prefix="/api/title")
-
 
 @router.get("/list")
 def title_list(user_input: str):
@@ -43,10 +43,53 @@ def title_list(user_input: str):
         if sorted_similarity and sorted_similarity[0][0] == user_input:
             sorted_similarity.pop(0)
 
-        # 상위 5개 결과 반환
-        result = dict(sorted_similarity[:5])
+        # 상위 5개 결과 반환 (벡터값을 포함하지 않음)
+        result = {
+            "similar_titles": [{
+                "title": title,
+                "similarity": similarity
+            } for title, similarity in sorted_similarity[:5]]
+        }
     else:
         result = {"message": "작품을 찾을 수 없습니다. 줄거리를 입력해주세요."}
+
+    # 데이터베이스 세션 닫기
+    db.close()
+
+    return result
+
+# 새로운 엔드포인트를 정의합니다.
+@router.get("/recommend")
+def recommend_titles(summary_input: str):
+    # 데이터베이스 세션 열기
+    db = SessionLocal()
+
+    # 스토리 요약을 입력으로 받고 벡터를 계산합니다.
+    model = SentenceTransformer('snunlp/KR-SBERT-V40K-klueNLI-augSTS')
+    input_vector = model.encode(summary_input)
+
+    # 모든 작품들과의 유사도를 계산하고 결과를 저장할 딕셔너리를 생성합니다.
+    similarity_dict = {}
+
+    titles = db.query(Title).all()
+
+    for title in titles:
+        vector_str = title.vector  # 데이터베이스에서 문자열 형식의 벡터 데이터 가져오기
+        vector = np.array(json.loads(vector_str))  # JSON 문자열을 NumPy 배열로 변환
+        vector = torch.from_numpy(vector).float()
+        similarity = util.cos_sim(input_vector, vector)
+        similarity_dict[title.title] = similarity
+
+    # 결과를 유사도 높은 순으로 정렬
+    sorted_similarity = sorted(similarity_dict.items(), key=lambda x: x[1], reverse=True)
+
+    # 상위 5개 결과 반환
+    result = {
+        "recommended_titles": [{
+            "title": title,
+            "similarity": similarity
+        } for title, similarity in sorted_similarity[:5]]
+    }
 
     # 데이터베이스 세션 닫기
     db.close()
